e754f0bf449a6f67a4d144cd4ab2a2d4f53fd414
Alex Suatac <alexsuatac@gmail.com>
2022-12-16 21:51:34 -0500

    Day 10 part 1 - simple but lots of memory

--- Day10/Day10.cpp
+++ Day10/Day10.cpp
@@ -3,10 +3,9 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-
 #include <vector>
 
-static long X = 0; // global var for the register
+static long X = 1; // global var for the register
 struct instruction {
     std::string instruction;
     int val;
@@ -15,13 +14,12 @@ struct instruction {
 static std::vector<instruction> instructions = {};
 struct state {
     std::string instruction;
-    unsigned numCyclesToComplete;
+    unsigned cycleN;
     long XatCycleN;
 };
  // store the instruction, cycle at which it was
  // executed and the value of X for each step of the program
 static std::vector<state> memory = {};
-
 
 // split a string up
 std::vector<std::string> split(const std::string& str) {
@@ -33,6 +31,40 @@ std::vector<std::string> split(const std::string& str) {
     }
 
     return result;
+}
+
+// process the code
+void runProcess() {
+    unsigned cycle = 0;
+
+    // store the initial state
+    state tmp = {.instruction = "initial", .cycleN = cycle, .XatCycleN = X};
+    memory.push_back(tmp);
+
+    for(auto const& line : instructions) {
+        if(line.instruction == "noop") {
+            cycle += 1;
+            memory.push_back({.instruction = line.instruction, .cycleN = cycle, .XatCycleN = X});
+        } else {
+            // store the state at the first clock
+            cycle += 1;
+            memory.push_back({.instruction = line.instruction, .cycleN = cycle, .XatCycleN = X});
+            
+            // update the register and store the state with the second clock
+            cycle += 1;
+            X += line.val;
+            memory.push_back({.instruction = line.instruction, .cycleN = cycle, .XatCycleN = X});
+        }
+    }
+}
+
+int getSumOfSignalStrengths() {
+    return 20 * memory[19].XatCycleN +
+            60 * memory[59].XatCycleN +
+            100 * memory[99].XatCycleN +
+            140 * memory[139].XatCycleN +
+            180 * memory[179].XatCycleN +
+            220 * memory[219].XatCycleN;
 }
 
 int main() {
@@ -52,6 +84,9 @@ int main() {
             instructions.push_back({.instruction = parsed[0], .val = std::stoi(parsed[1])});
         }
     }
+
+    runProcess();
+    std::cout << "part 1: " << getSumOfSignalStrengths() << std::endl;
 
     ifs.close();
     return 0;
